76264f91491bde472865442fcc1470abea9226af
Esteban Betancourt <stebanbetancourt100@gmail.com>
2025-01-30 13:25:54 -0500

    cambio 2

---
+++ nombres y apellidos.py
@@ -0,0 +1,8 @@
+# Archivo: alexanderbetancourt.py
+# Creado por: Alexander Betancourt
+
+def producto_serie(a, b):
+    """
+    Retorna el producto de dos números como parte de una serie.
+    """
+    return a * b
