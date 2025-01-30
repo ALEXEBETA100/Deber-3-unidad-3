2c5c508f4100e07e941dc2704ace6d970048ddba
Esteban Betancourt <stebanbetancourt100@gmail.com>
2025-01-30 13:21:34 -0500

    cambio 1

---
+++ series.py
@@ -0,0 +1,19 @@
+# Proyecto de Desarrollo
+# Resultados de aprendizaje: 
+# Administrar la gestión de código colaborativo
+
+# Descripción de la tarea:
+# - Realizar las explicaciones de video
+# - Modificar el archivo de series.py y poner una función. Indicar los comentarios con sus nombres y apellidos.
+# - Agregar un archivo nuevo de extensión .py con su nombreapellido.py
+
+# Archivo: series.py
+# Modificado por: Alexander Betancourt
+
+def suma_serie(a, b):
+    """
+    Retorna la suma de dos números como parte de una serie.
+    """
+    return a + b
+
+# Fin de la modficacion
